96df246280e83e052f92daf690e6e4aea68107c6
RoxanaCamara <roxana.camara07@gmail.com>
2024-07-23 17:25:10 -0300

    feat: new scketch

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import Scketch_02 from "./components/Scketch_02";
 import Scketch_03 from "./components/Scketch_03";
 import Scketch_04 from "./components/Scketch_04";
 import Scketch_05 from "./components/Scketch_05";
+import Scketch_06 from "./components/Scketch_06";
 
 function App() {
-	return (
-		<>
-			<Scketch_02 />
-			{/***
+  return (
+    <>
+      <Scketch_06 />
+      {/***
 			<Scketch_01 />
 			<Scketch_03 />
 			<Scketch_04 />
 			<Scketch_05 /> 
 			*/}
-		</>
-	);
+    </>
+  );
 }
 
 export default App;
